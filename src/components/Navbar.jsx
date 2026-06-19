36273292c668a05a3e4be7d7367916cccbcb5338
Romherre <98140472+Romherre@users.noreply.github.com>
2026-06-19 19:02:40 -0300

    se pasa a responsive: About + Navbar con menu hamburguesa

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,11 @@
 import '../styles/Navbar.css'
 import logo from '../assets/red-reparar-logo.png'
+import { useState } from 'react'
+import { Menu, X } from 'lucide-react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <header className="header">
       <nav className="navbar">
@@ -9,7 +13,12 @@ const Navbar = () => {
           <img src={logo} alt="Logo Red Reparar" />
           RED REPARAR
         </a>
-        <div className="navbar__menu">
+
+        <button className="navbar__toggle" onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X size={28} /> : <Menu size={28} />}
+        </button>
+
+        <div className={`navbar__menu ${isOpen ? 'navbar__menu--open' : ''}`}>
           <ul className="navbar__links">
             <li><a href="#" className='inicio'>INICIO</a></li>
             <li><a href="#nosotros">NOSOTROS</a></li>
