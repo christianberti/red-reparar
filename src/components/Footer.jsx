cc87373e4c5a1d4b20931c5279fb8087d5312b21
Romherre <98140472+Romherre@users.noreply.github.com>
2026-08-03 07:12:50 -0300

    Se modifica tamao de icono

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,8 +21,8 @@ const Footer = () => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="18"
-                height="18"
+                width="22"
+                height="22"
                 viewBox="0 0 24 24"
                 fill="none"
                 stroke="currentColor"
